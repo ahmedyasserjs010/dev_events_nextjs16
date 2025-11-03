b4591e3d929edb2558bb74bb9c7a454ecc8f086c
AhmedYasser_JS01 <ahmedyasser.js01@gmail.com>
2025-11-03 16:06:57 +0200

    fix events

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import EventCard from '@/components/EventCard';
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
 
-// import events from "@/lib/constants";
+
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
